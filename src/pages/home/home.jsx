4611b869fb25d74f0242c9c6bdc25a14e9b59392
marcos <marcoshr_baptista@hotmail.com>
2024-08-29 10:06:41 -0300

    first commit 3

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,7 +5,7 @@ import IconPerson from './assets/icon _person_.svg';
 import IconExit from './assets/exit.svg';
 import IconSettings from './assets/settings.svg';
 
-import Trackings from '../../Components/trackings/trackings'
+import Trackings from '../../components/trackings/trackings'
 import Sidebar from '../../components/sidebar/sidebar'
 import FlipCardSmall from '../../components/flip_card_small/flip_card_small';
 
@@ -20,7 +20,7 @@ export default function Home() {
     return (
         <div className={style.HomePage}>
             <img src={LogoBlack} alt='Logo' className={style.Logo_Home} />
-            <Trackings />
+            <Trackings/>
             <div className={style.nav__Options}>
                 <nav>
                     <ul className={style.lista_sidebar_options}>
