baa90b0f556ef4e6d99575387a8e921e91ba4de5
marcos <marcoshr_baptista@hotmail.com>
2024-09-16 08:44:34 -0300

    vercel.json python 5

--- python/app.py
+++ python/app.py
@@ -25,4 +25,4 @@ def receive_json():
     return jsonify({'error': 'Invalid tracking type'}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
