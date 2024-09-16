0406d948ad89c0570cb3528aac09b77c2439573d
marcos <marcoshr_baptista@hotmail.com>
2024-09-16 09:52:47 -0300

    vercel.json python 11 now is working

--- frontend/src/components/camera/camera.jsx
+++ frontend/src/components/camera/camera.jsx
@@ -78,7 +78,7 @@ export default function Camera() {
 
         if(isChecked)
         {
-            fetch('http://127.0.0.1:5000', {  
+            fetch('https://vite-app-tcc-senai-2q6k.vercel.app/', {  
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- frontend/src/services/pythonApi.jsx
+++ frontend/src/services/pythonApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const pythonApi = axios.create({
-    baseURL:"http://127.0.0.1:5000",
+    baseURL:"https://vite-app-tcc-senai-2q6k.vercel.app/",
 
 });
 
