9a90a1931fc920c6f445cbe1e983232c5e82b8f6
ilunnie <luisgustavocaris2004@gmail.com>
2024-09-26 16:58:16 -0300

    feature: mouse click with hand tracking

--- python/app/trackers/hand_commands/mouse_commands.py
+++ python/app/trackers/hand_commands/mouse_commands.py
@@ -5,6 +5,7 @@ import tracking as tck
 from app.command_label import command
 from app.system_infos import System
 from tracking.hand_tracking import Hand
+from tracking.utils import math
 from typing import List
 
 from ..abstract import AbstractTracking
@@ -12,6 +13,11 @@ from ..abstract import AbstractTracking
 pyautogui.FAILSAFE = False
 
 x, y = (0, 0)
+primary_btn = False
+secondary_btn = False
+
+previous_primary_btn = False
+previous_secondary_btn = False
 
 @command
 def _mouse_move(cls: AbstractTracking, hands: List[Hand], image: mp.Image, timestamp: int):
@@ -36,10 +42,52 @@ def _mouse_move(cls: AbstractTracking, hands: List[Hand], image: mp.Image, times
     y = posy + normy * height
 
 @command
+def _mouse_click(cls: AbstractTracking, hands: List[Hand], image: mp.Image, timestamp: int):
+    if not hands:
+        return
+    
+    mouse_hand = next((hand for hand in hands if hand.side == tck.side.RIGHT), hands[0])
+
+    thumb_tip_idx = tck.finger.get_tip(tck.finger.THUMB)
+    index_tip_idx = tck.finger.get_tip(tck.finger.INDEX)
+    middle_tip_idx = tck.finger.get_tip(tck.finger.MIDDLE)
+
+    thumb_tip, index_tip, middle_tip = mouse_hand.landmarks.get_points([thumb_tip_idx, index_tip_idx, middle_tip_idx])
+
+    global primary_btn, secondary_btn
+    if 0 <= thumb_tip[0] <= 1 and 0 <= thumb_tip[1] <= 1:
+        primary_btn = (0 <= index_tip[0] <= 1 and 
+                       0 <= index_tip[1] <= 1 and 
+                       math.euclidean_distance(thumb_tip, index_tip) < 0.07)
+        
+        secondary_btn = (0 <= middle_tip[0] <= 1 and 
+                       0 <= middle_tip[1] <= 1 and 
+                       math.euclidean_distance(thumb_tip, middle_tip) < 0.07)
+    else:
+        primary_btn = False
+        secondary_btn = False
+
+@command
 def _mouse_set_status(cls: AbstractTracking, *args):
     if not cls.hands:
         return
+    global previous_primary_btn, previous_secondary_btn
     
-    pyautogui.moveTo(x, y)
+    if primary_btn == previous_primary_btn and secondary_btn == previous_secondary_btn:
+        pyautogui.moveTo(x, y)
+        return
+    
+    if primary_btn and not previous_primary_btn:
+        pyautogui.mouseDown(x, y)
+    elif not primary_btn and previous_primary_btn:
+        pyautogui.mouseUp(x, y)
 
-__all__ = [_mouse_move, _mouse_set_status]
+    if secondary_btn and not previous_secondary_btn:
+        pyautogui.mouseDown(x, y, button='right')
+    elif not secondary_btn and previous_secondary_btn:
+        pyautogui.mouseUp(x, y, button='right')
+
+    previous_primary_btn = primary_btn
+    previous_secondary_btn = secondary_btn
+
+__all__ = [_mouse_move, _mouse_click, _mouse_set_status]
