35730e9cecff29fcd70669580a7fc6a1c46f9748
ilunnie <luisgustavocaris2004@gmail.com>
2024-09-26 16:57:51 -0300

    feature: possibility of choosing whether command is sequential

--- python/app/command_label.py
+++ python/app/command_label.py
@@ -1,14 +1,18 @@
 from functools import wraps
-from threading import Thread
+from threading import Lock, Thread
 
-def command(func):
+def command(func, sequential: bool = False):
+    lock = Lock()
     @wraps(func)
     def wrapper(*args, **kwargs):
         def run_func():
-            func(*args, **kwargs)
+            with lock:
+                func(*args, **kwargs)
         
-        thread = Thread(target=run_func)
-        thread.start()
-        return thread
+        if sequential or not lock.locked():
+            thread = Thread(target=run_func)
+            thread.start()
+            return thread
+        return None
     wrapper.is_command = True
     return wrapper
