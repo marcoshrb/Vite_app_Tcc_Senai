07968306872991a740386869a943dc42d0281afe
ilunnie <luisgustavocaris2004@gmail.com>
2024-09-30 10:47:54 -0300

    feat: draw interaction area in webcam

--- python/webcam.py
+++ python/webcam.py
@@ -2,6 +2,7 @@ import cv2
 
 from app.events import loading
 from tracking import CONFIG
+from tracking.utils import normalize_pixel
 from config import Config
 
 hand_config = Config('HandTracking')
@@ -12,6 +13,7 @@ def main():
     cap = CONFIG.VIDEO_CAPTURE
     while cap.isOpened:
         frame = cap.frame
+        width, height = cap.shape
         if frame is None:
             continue
         
@@ -19,6 +21,10 @@ def main():
             from app.trackers.hand_tracking import HandTracking
             for hand in HandTracking.hands:
                 hand.draw(frame, (255, 0, 0))
+                cv2.circle(frame, normalize_pixel(*hand.center_palm()[:2], width, height), 5, (0, 0, 255), -1)
+            
+            (minx, maxx), (miny, maxy) = hand_config['interaction_area'].values()
+            cv2.rectangle(frame, (int(width * minx), int(height * miny)), (int(width * maxx), int(height * maxy)), (0, 255, 0))
                 
         cv2.imshow("Camera", frame)
         cv2.waitKey(10)
